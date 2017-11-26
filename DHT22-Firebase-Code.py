039dc19a39d04b52eb9cc4efd04b3d1a2f6a7c18
AMHD <30323515+AMHD@users.noreply.github.com>
2017-11-26 15:34:04 +0300

    Add files via upload

---
+++ DHT22-Firebase-Code.py
@@ -0,0 +1,23 @@
+import pyrebase
+import sys
+import Adafruit_DHT
+import time
+
+from firebase import firebase
+config = {
+  "apiKey": "AIzaSyD68dO5eqaIPqa4KBm9DE0kt5U-aee2ooA",
+  "authDomain": "tempapp-a0c0e.firebaseapp.com",
+  "databaseURL": "https://tempapp-a0c0e.firebaseio.com",
+  "storageBucket": "tempapp-a0c0e"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+while 1:
+        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+        time_hhmmss = time.strftime('%H:%M:%S')
+        date_mmddyyyy = time.strftime('%d/%m/%Y')
+        data = {"Date": date_mmddyyyy,"Time": time_hhmmss, "Temperature": temperature, "Humidity": humidity}
+        db.child("/message").push(data)
+        print("Temp: %f -- Date: %s  -- Time: %s" %(x,date_mmddyyyy,time_hhmmss))
+        time.sleep(60)
